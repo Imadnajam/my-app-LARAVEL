05f28083562b57214864e109c08aae5b027f6350
Imad <118115991+Imadnajam@users.noreply.github.com>
2023-11-28 17:53:07 +0100

    Update 2019_12_14_000001_create_personal_access_tokens_table.php

--- lara/my-app-1/database/migrations/2019_12_14_000001_create_personal_access_tokens_table.php
+++ lara/my-app-1/database/migrations/2019_12_14_000001_create_personal_access_tokens_table.php
@@ -11,6 +11,7 @@ return new class extends Migration
      *
      * @return void
      */
+    
     public function up()
     {
         Schema::create('personal_access_tokens', function (Blueprint $table) {
